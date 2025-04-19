5f1694193bbe1e5672a47ec4dbb66afb399884d6
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-19 10:17:24 +0800

    add parseDate

---
+++ src/date/parse-date.ts
@@ -0,0 +1,32 @@
+import { AmPm, ParsedDate } from './date.types';
+
+const regexp = /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2}):(\d{2}) (\w{2})/;
+
+export function parseDate(date: Date): ParsedDate | undefined {
+  if (isNaN(+date)) return;
+
+  const match = new Intl.DateTimeFormat('en-US', {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  })
+    .format(date)
+    .match(regexp);
+  if (match?.length === 8) {
+    const [, month, day, year, hour12, minute, second, ampm] = match as [
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      AmPm
+    ];
+    return { year, month, day, hour12, minute, second, ampm };
+  }
+}
--- src/parser/date-parser.ts
+++ src/parser/date-parser.ts
@@ -1,38 +1,10 @@
-import { AmPm } from '../date/date.types.js';
+import { parseDate } from '../date/parse-date.js';
 import { InternalParser } from './parser.types.js';
-
-const regexp = /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2}):(\d{2}) (\w{2})/;
 
 export const dateParser: InternalParser = {
   name: 'date',
   formats: ['Any valid date string for the Date() constructor'],
   parse(fileName) {
-    const date = new Date(fileName);
-    if (isNaN(+date)) return;
-
-    const match = new Intl.DateTimeFormat('en-US', {
-      month: '2-digit',
-      day: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true
-    })
-      .format(date)
-      .match(regexp);
-    if (match?.length === 8) {
-      const [, month, day, year, hour12, minute, second, ampm] = match as [
-        string,
-        string,
-        string,
-        string,
-        string,
-        string,
-        string,
-        AmPm
-      ];
-      return { year, month, day, hour12, minute, second, ampm };
-    }
+    return parseDate(new Date(fileName));
   }
 };
